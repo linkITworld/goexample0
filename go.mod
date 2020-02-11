0201eb243e79835baad8b70fee319f17134a41e1
陈思琳 <chensilin@chensilindeMacBook-Air.local>
2020-02-12 00:55:45 +0800

    依赖gin包的初步学习

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/linkITworld/goexample0
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.5.0
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/linkITworld/goexample0/model"
 )
 
@@ -13,4 +15,23 @@ func main() {
 	people.Name = "strr"
 	people.UUID = "sty"
 	fmt.Println(people)
+
+	router := gin.Default()
+
+	// This handler will match /user/john but will not match /user/ or /user
+	router.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	// However, this one will match /user/john/ and also /user/john/send
+	// If no other routers match /user/john, it will redirect to /user/john/
+	router.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
+	router.Run(":8080")
 }
