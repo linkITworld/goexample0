7b202340ef130fdb9e65dd07860c803d8615ac61
陈思琳 <chensilin@chensilindeMacBook-Air.local>
2020-02-13 23:11:52 +0800

    自定义包的使用

---
+++ 02_package/major/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/linkITworld/goexample0/02_package/stringutil"
+	"github.com/linkITworld/goexample0/02_package/winniepooh"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG,olleH"))
+	fmt.Println(stringutil.Myname)
+	fmt.Println(winniepooh.BearName)
+}
